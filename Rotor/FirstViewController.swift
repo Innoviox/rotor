fc8b890f5c838e3e273f84fbbbf52cc253c86943
Innoviox <sarras305@gmail.com>
2018-05-03 17:05:08 -0400

    Added multiple inputs

--- Rotor/FirstViewController.swift
+++ Rotor/FirstViewController.swift
@@ -12,9 +12,16 @@ class FirstViewController: UIViewController {
 
     @IBOutlet weak var control: UISegmentedControl!
     @IBOutlet weak var addButton: UIButton!
+    @IBOutlet weak var originalText: UITextField!
+    
+    var types = [String](arrayLiteral: "Pattern", "Anagram", "Build");
+    var textFields = [UITextField]();
     
     override func viewDidLoad() {
         super.viewDidLoad()
+        // textFields.append(originalText);
+        update(originalText)
+        originalText.addTarget(self, action: #selector(FirstViewController.typed(_:)), for: UIControlEvents.editingChanged)
         // Do any additional setup after loading the view, typically from a nib.
     }
 
@@ -25,24 +32,35 @@ class FirstViewController: UIViewController {
 
 
     @IBAction func typed(_ sender: Any) {
+        for tf: UITextField in  self.textFields {
+            print(tf.text!)
+        }
     }
     
     @IBAction func `switch`(_ sender: Any) {
-        
+        print(self.types[self.control.selectedSegmentIndex])
     }
     
     @IBAction func add(_ sender: Any) {
-        let sampleTextField =  UITextField(frame: CGRect(x: 20, y: 100, width: 300, height: 40))
-        sampleTextField.placeholder = "Enter text here"
-        sampleTextField.font = UIFont.systemFont(ofSize: 15)
-        sampleTextField.borderStyle = UITextBorderStyle.roundedRect
-        sampleTextField.autocorrectionType = UITextAutocorrectionType.no
-        sampleTextField.keyboardType = UIKeyboardType.default
-        sampleTextField.returnKeyType = UIReturnKeyType.done
-        sampleTextField.clearButtonMode = UITextFieldViewMode.whileEditing;
-        sampleTextField.contentVerticalAlignment = UIControlContentVerticalAlignment.center
-        sampleTextField.delegate = self as? UITextFieldDelegate
-        self.view.addSubview(sampleTextField)
+        let frame = self.originalText.frame
+        let h = frame.size.height
+        let newField =  UITextField(frame: CGRect(x: frame.origin.x, y: CGFloat(frame.origin.y) + (5 + h) * CGFloat(self.textFields.count), width: frame.size.width, height: h))
+        update(newField)
+        newField.addTarget(self, action: #selector(FirstViewController.typed(_:)), for: UIControlEvents.editingChanged)
+    }
+    
+    func update(_ field: UITextField) {
+        field.placeholder = "Enter text here"
+        field.font = UIFont.systemFont(ofSize: 15)
+        field.borderStyle = UITextBorderStyle.roundedRect
+        field.autocorrectionType = UITextAutocorrectionType.no
+        field.keyboardType = UIKeyboardType.default
+        field.returnKeyType = UIReturnKeyType.done
+        field.clearButtonMode = UITextFieldViewMode.whileEditing;
+        field.contentVerticalAlignment = UIControlContentVerticalAlignment.center
+        field.delegate = self as? UITextFieldDelegate
+        self.view.addSubview(field)
+        textFields.append(field)
     }
 }
 
