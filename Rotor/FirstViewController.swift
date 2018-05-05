1d1bf57767daeeb1aa5cc4c4c3cd23a6185cd1c2
Innoviox <sarras305@gmail.com>
2018-05-05 16:28:59 -0400

    Anagram code

--- Rotor/FirstViewController.swift
+++ Rotor/FirstViewController.swift
@@ -13,8 +13,7 @@ class FirstViewController: UIViewController {
     @IBOutlet weak var control: UISegmentedControl!
     @IBOutlet weak var addButton: UIButton!
     @IBOutlet weak var originalText: UITextField!
-    @IBOutlet weak var output: UIScrollView!
-    @IBOutlet weak var label: UILabel!
+    @IBOutlet weak var label: UITextView!
     
     var types = ["Pattern", "Anagram", "Build"];
     
@@ -22,19 +21,20 @@ class FirstViewController: UIViewController {
     var minusButtons = [UIButton]();
     var controls = [UISegmentedControl]();
     var dictionaries = [String: Set<String>]()
+    let alph = "ABCDEFGHIJKLMNOPQRSTUVWXYZ"
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        update(originalText)
+        field_set(originalText)
         originalText.addTarget(self, action: #selector(FirstViewController.typed(_:)), for: UIControlEvents.editingChanged)
         print("Loading")
-        let alph = "ABCDEFGHIJKLMNOPQRSTUVWXYZ"
+        
         let filePath = Bundle.main.resourcePath!
         for char1 in alph {
             for char2 in alph {
                 let diphth = String(char1) + String(char2)
                 dictionaries[diphth] = Set<String>();
-                let reader = StreamReader(path: filePath + "/" + diphth + ".txt");
+                let reader = StreamReader(path: filePath + "/" + diphth + ".txt")
                 while let line = reader?.nextLine() {
                     dictionaries[diphth]?.insert(line)
                 }
@@ -50,19 +50,22 @@ class FirstViewController: UIViewController {
         super.didReceiveMemoryWarning()
     }
 
+    func update() {
+        let words = search(mode: types[control.selectedSegmentIndex], text: originalText!.text!)
+        label.text = words.joined(separator: "\n")
+    }
+    
     @IBAction func typed(_ sender: Any) {
         for tf: UITextField in  self.textFields {
-            print(tf.text!)
+            // print(tf.text!)
+            tf.text = tf.text?.uppercased()
         }
-        label.text = ""
-        for word in search(mode: types[control.selectedSegmentIndex], text: originalText!.text!) {
-            label.text = (label.text ?? "") + word + "\n"
-            print(word)
-        }
+        update()
     }
     
     @IBAction func `switch`(_ sender: Any) {
         print(self.types[self.control.selectedSegmentIndex])
+        update()
     }
     
     @IBAction func add(_ sender: Any) {
@@ -77,7 +80,7 @@ class FirstViewController: UIViewController {
             self.controls.append(control)
             
             let newField = UITextField(frame: CGRect(x: frame.origin.x + 50, y: CGFloat(frame.origin.y) + (5 + h) * CGFloat(self.textFields.count), width: frame.size.width - 50, height: h))
-            update(newField)
+            field_set(newField)
             newField.addTarget(self, action: #selector(FirstViewController.typed(_:)), for: UIControlEvents.editingChanged)
         
             frame = self.addButton.frame
@@ -87,6 +90,9 @@ class FirstViewController: UIViewController {
             newButton.addTarget(self, action: #selector(self.deleteLine(_:)), for: .touchUpInside)
             self.view.addSubview(newButton)
             self.minusButtons.append(newButton)
+            
+            frame = self.label.frame
+            self.label.frame = CGRect(x: frame.origin.x, y: frame.origin.y + h + 5, width: frame.size.width, height: frame.size.height - h - 5)
         }
     }
     
@@ -123,7 +129,7 @@ class FirstViewController: UIViewController {
         }
     }
     
-    func update(_ field: UITextField) {
+    func field_set(_ field: UITextField) {
         field.placeholder = "Enter text here"
         field.font = UIFont.systemFont(ofSize: 15)
         field.borderStyle = UITextBorderStyle.roundedRect
@@ -138,20 +144,81 @@ class FirstViewController: UIViewController {
     }
     
     func search(mode: String, text: String) -> [String] {
+        if (text.count < 2) { return [String]() }
         var ret = [String]()
         if mode == self.types[0] {
             let pattern = "^" + text.uppercased() + "$"
             for (_, set) in self.dictionaries {
                 for word in set {
-                    let regex = try! NSRegularExpression(pattern: pattern, options: [])
-
-                    if (regex.matches(in: word, options: [], range: NSRange(location: 0, length: word.count)).count > 0) {
+                    if word.range(of: pattern, options: .regularExpression, range: nil, locale: nil) != nil {
                         ret.append(word)
+                    }
+                }
+            }
+        } else if mode == self.types[1] {
+            let perms = permute(items: text)
+            for word in perms.map({ String($0) }) {
+                for bWord in blanks(word) {
+                    if check(word: bWord) {
+                        ret.append(bWord)
                     }
                 }
             }
         }
         
+        return ret.sorted()
+    }
+    
+    func check(word: String) -> Bool {
+        let index = word.index(word.startIndex, offsetBy: 2)
+        return dictionaries[String(word[..<index])]!.contains(word)
+    }
+    
+    // Takes any collection of T and returns an array of permutations
+    func permute<C: Collection>(items: C) -> [[C.Iterator.Element]] {
+        var scratch = Array(items) // This is a scratch space for Heap's algorithm
+        var result: [[C.Iterator.Element]] = [] // This will accumulate our result
+        
+        // Heap's algorithm
+        func heap(_ n: Int) {
+            if n == 1 {
+                result.append(scratch)
+                return
+            }
+            
+            for i in 0..<n-1 {
+                heap(n-1)
+                let j = (n%2 == 1) ? 0 : i
+                scratch.swapAt(j, n-1)
+            }
+            heap(n-1)
+        }
+        
+        // Let's get started
+        heap(scratch.count)
+        
+        // And return the result we built up
+        return result
+    }
+    
+    func blanks(_ word: String) -> [String] {
+        print("recieving \(word)")
+        var ret = [String]()
+        if word.count > 6 {return ret}
+        if word.contains(".") {
+            let idx = word.index(of: ".")
+            for char in self.alph {
+                // if (int_idx == 0) {
+                //     ret.append(contentsOf: self.blanks(String(char) + word.suffix(1)))
+                // } else if (int_idx == word.count - 1) {
+                //     ret.append(contentsOf: self.blanks(word.prefix(word.count - 1) + String(char)))
+                // } else {
+                    ret.append(contentsOf: self.blanks(word.replacingCharacters(in: idx!...idx!, with: String(char))))
+                // }
+            }
+        } else {
+            ret.append(word)
+        }
         return ret
     }
 }
